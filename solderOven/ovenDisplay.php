d862ebee88eab17e611db5990c647a8592a0384e
billy0705 <billyg123@gmail.com>
2023-05-02 15:24:19 +0700

    connect api

--- solderOven/ovenDisplay.php
+++ solderOven/ovenDisplay.php
@@ -15,17 +15,38 @@
 
         table {
             border-collapse: collapse;
+            border-spacing: 5px;
+            /* border: none; */
         }
 
         th,
         td {
-            border: 1px solid #ddd;
-            padding: 8px;
+            /* border: 1px solid #ddd; */
+            border: none;
+            padding: 10px;
             text-align: center;
+            vertical-align: middle;
+            background-color: #32CD32;
         }
 
         th {
-            background-color: #f2f2f2;
+            /* background-color: #f2f2f2; */
+            font-size: 1.5em;
+            font-weight:bold;
+            background-color: #00FFFF; 
+        }
+        thead {
+            border: 1px solid #000000;
+        }
+        body {
+            background-color: #32CD32;
+        }
+        td span {
+            display: inline-block;
+            padding: 2px 5px;
+            background-color: #fff;
+            height: 50%; 
+            width: 70%;
         }
     </style>
     </style>
@@ -34,26 +55,40 @@
 <body>
     <?php require "header.php"; ?>
     <?php
-        $url = 'http://10.10.2.108/Solder/api/JT_WS450/JT_WS450Values';
-        $json = file_get_contents($url);
-        $obj = json_decode($json);
-        if ($obj->statusCode == 200){
-            $data = json_decode(json_encode($obj->jT_WS450_Values[0]), true);
-            $tsv = $data["transportSpeedValue"];
-            $rwv = $data["railWidthValue"];
-            $ph1bv = $data["preheater1BValue"];
-            $ph2bv = $data["preheater2BValue"];
-            $ph3bv = $data["preheater3BValue"];
-            $ph1tv = $data["preheater1TValue"];
-            $ph2tv = $data["preheater2TValue"];
-            $ph3tv = $data["preheater3TValue"];
-            $spv = $data["solderPotValue"];
-            $w1v = $data["wave1Value"];
-            $w2v = $data["wave2Value"];
-        }
+    $machineID = '40001';
+    $configurl = "http://10.10.2.108/Solder/api/JT_WS450/JT_WS450Config/" . $machineID;
+    $json = file_get_contents($configurl);
+    $obj = json_decode($json);
+    if ($obj->statusCode == 200) {
+        $machineConfig = json_decode(json_encode($obj->jT_WS450_Configs[0]), true);
+    }
+    $locationid = $machineConfig["locationID"];
+    $locationurl = "http://10.10.2.108/Solder/api/JT_WS450/Location/" . $locationid;
+    $json = file_get_contents($locationurl);
+    $obj = json_decode($json);
+    if ($obj->statusCode == 200){
+        $locationname = json_decode(json_encode($obj->locationConfigs[0]), true)["locationName"];
+    }
+    $url = 'http://10.10.2.108/Solder/api/JT_WS450/JT_WS450Values';
+    $json = file_get_contents($url);
+    $obj = json_decode($json);
+    if ($obj->statusCode == 200) {
+        $data = json_decode(json_encode($obj->jT_WS450_Values[0]), true);
+        $tsv = $data["transportSpeedValue"];
+        $rwv = $data["railWidthValue"];
+        $ph1bv = $data["preheater1BValue"];
+        $ph2bv = $data["preheater2BValue"];
+        $ph3bv = $data["preheater3BValue"];
+        $ph1tv = $data["preheater1TValue"];
+        $ph2tv = $data["preheater2TValue"];
+        $ph3tv = $data["preheater3TValue"];
+        $spv = $data["solderPotValue"];
+        $w1v = $data["wave1Value"];
+        $w2v = $data["wave2Value"];
+    }
 
     ?>
-    <!-- <h1>Solder Wave Machine 監控</h1> -->
+    <!-- <h1>Solder Wave Machine</h1> -->
     <div style="display:flex; height:100vh">
         <table style="width:50vw; height:100%; margin: 5px;">
             <thead>
@@ -67,68 +102,68 @@
             <tbody>
                 <tr>
                     <td>Preheat B1</td>
-                    <td>100</td>
-                    <td><?php echo $ph1bv;?></td>
+                    <td><span><?php echo $machineConfig["preheater1BSetting"]; ?></span></td>
+                    <td><span><?php echo $ph1bv; ?></span></td>
                     <td>Deg C</td>
                 </tr>
                 <tr>
                     <td>Preheat B2</td>
-                    <td>120</td>
-                    <td><?php echo $ph2bv;?></td>
+                    <td><span><?php echo $machineConfig["preheater2BSetting"]; ?></span></td>
+                    <td><span><?php echo $ph2bv; ?></span></td>
                     <td>Deg C</td>
                 </tr>
                 <tr>
                     <td>Preheat B3</td>
-                    <td>120</td>
-                    <td><?php echo $ph3bv;?></td>
+                    <td><span><?php echo $machineConfig["preheater3BSetting"]; ?></span></td>
+                    <td><span><?php echo $ph3bv; ?></span></td>
                     <td>Deg C</td>
                 </tr>
                 <tr>
                     <td>Preheat T2</td>
-                    <td>120</td>
-                    <td><?php echo $ph2tv;?></td>
+                    <td><span><?php echo $machineConfig["preheater2TSetting"]; ?></span></td>
+                    <td><span><?php echo $ph2tv; ?></span></td>
                     <td>Deg C</td>
                 </tr>
                 <tr>
                     <td>Preheat T3</td>
-                    <td>120</td>
-                    <td><?php echo $ph3tv;?></td>
+                    <td><span><?php echo $machineConfig["preheater3TSetting"]; ?></span></td>
+                    <td><span><?php echo $ph3tv; ?></span></td>
                     <td>Deg C</td>
                 </tr>
                 <tr>
                     <td>Solder Pot</td>
-                    <td>250</td>
-                    <td><?php echo $spv;?></td>
+                    <td><span><?php echo $machineConfig["solderPotSetting"]; ?></span></td>
+                    <td><span><?php echo $spv; ?></span></td>
                     <td>Deg C</td>
                 </tr>
                 <tr>
                     <td>Conveyor</td>
-                    <td>1200</td>
-                    <td><?php echo $tsv;?></td>
+                    <td><span><?php echo $machineConfig["transportSpeedSetting"]; ?></span></td>
+                    <td><span><?php echo $tsv; ?></span></td>
                     <td>mm/min</td>
                 </tr>
                 <tr>
                     <td>Rail Width</td>
-                    <td>347</td>
-                    <td><?php echo $rwv;?></td>
+                    <td><span><?php echo $machineConfig["railWidthSetting"]; ?></span></td>
+                    <td><span><?php echo $rwv; ?></span></td>
                     <td>mm</td>
                 </tr>
                 <tr>
                     <td>Wave1</td>
-                    <td>12</td>
-                    <td><?php echo $w1v;?></td>
+                    <td><span><?php echo $machineConfig["wave1Setting"]; ?></span></td>
+                    <td><span><?php echo $w1v; ?></span></td>
                     <td>Hz</td>
                 </tr>
                 <tr>
                     <td>Wave2</td>
-                    <td>24</td>
-                    <td><?php echo $w2v;?></td>
+                    <td><span><?php echo $machineConfig["wave2Setting"]; ?></span></td>
+                    <td><span><?php echo $w2v; ?></span></td>
                     <td>Hz</td>
                 </tr>
             </tbody>
         </table>
         <div style="width:50vw; height:100%">
-            <table style="height:30%; width:98%; margin: 5px;">
+            <table style="height:30%; width:98%; margin: 5px;" >
                 <thead>
                     <tr>
                         <th>Parameter Name</th>
@@ -138,20 +173,20 @@
                 <tbody>
                     <tr>
                         <td>Location Name</td>
-                        <td>PCBA Line 4</td>
+                        <td><?php echo $locationname; ?></td>
                     </tr>
                     <tr>
                         <td>Oven Model</td>
-                        <td>JT WS450</td>
+                        <td><?php echo $machineConfig["machineModel"]; ?></td>
                     </tr>
                     <tr>
                         <td>Time now</td>
-                        <td>2023-04-27 07:42:13 Thursday</td>
+                        <td id='Date'>2023-04-27 07:42:13 Thursday</td>
                     </tr>
-                    <tr>
+                    <!-- <tr>
                         <td>PCB in Count</td>
                         <td>349</td>
-                    </tr>
+                    </tr> -->
                 </tbody>
             </table>
             <svg viewBox="0 0 970 450">
@@ -159,19 +194,20 @@
                 <rect x="10" y="20" width="950" height="350" fill="#c7d1cc" stroke="#000" stroke-width="2" />
 
                 <!-- machine inner box -->
+                <!-- <?php if ($data["preheater1B"] == "Close") echo ' fill="gray"'; else echo ' fill="green"'?> -->
                 <rect id="preheatT1-rect" x="20" y="30" width="200" height="165" fill="gray" stroke="#000" stroke-width="2" />
-                <rect id="preheatB1-rect" x="20" y="195" width="200" height="165" fill="green" stroke="#000" stroke-width="2" />
-                <rect id="preheatT2-rect" x="230" y="30" width="200" height="165" fill="#e6e6e6" stroke="#000" stroke-width="2" />
-                <rect id="preheatB2-rect" x="230" y="195" width="200" height="165" fill="green" stroke="#000" stroke-width="2" />
-                <rect id="preheatT3-rect" x="440" y="30" width="200" height="165" fill="#e6e6e6" stroke="#000" stroke-width="2" />
-                <rect id="preheatB3-rect" x="440" y="195" width="200" height="165" fill="green" stroke="#000" stroke-width="2" />
-                <rect id="wave1-rect" x="650" y="30" width="150" height="165" fill="#e6e6e6" stroke="#000" stroke-width="2" />
-                <rect id="wave2-rect" x="800" y="30" width="150" height="165" fill="#e6e6e6" stroke="#000" stroke-width="2" />
-                <rect id="solderpot-rect"x="650" y="195" width="300" height="165" fill="green" stroke="#000" stroke-width="2" />
+                <rect id="preheatB1-rect" x="20" y="195" width="200" height="165" fill="<?php if ($data["preheater1B"] == "Close") echo '#e6e6e6'; else echo 'green'?>" stroke="#000" stroke-width="2" />
+                <rect id="preheatT2-rect" x="230" y="30" width="200" height="165" fill="<?php if ($data["preheater2T"] == "Close") echo '#e6e6e6'; else echo 'green'?>" stroke="#000" stroke-width="2" />
+                <rect id="preheatB2-rect" x="230" y="195" width="200" height="165" fill="<?php if ($data["preheater2B"] == "Close") echo '#e6e6e6'; else echo 'green'?>" stroke="#000" stroke-width="2" />
+                <rect id="preheatT3-rect" x="440" y="30" width="200" height="165" fill="<?php if ($data["preheater3T"] == "Close") echo '#e6e6e6'; else echo 'green'?>" stroke="#000" stroke-width="2" />
+                <rect id="preheatB3-rect" x="440" y="195" width="200" height="165" fill="<?php if ($data["preheater3B"] == "Close") echo '#e6e6e6'; else echo 'green'?>" stroke="#000" stroke-width="2" />
+                <rect id="wave1-rect" x="650" y="30" width="150" height="165" fill="<?php if ($data["wave1Value"] == "0.00") echo '#e6e6e6'; else echo 'green'?>" stroke="#000" stroke-width="2" />
+                <rect id="wave2-rect" x="800" y="30" width="150" height="165" fill="<?php if ($data["wave2Value"] == "0.00") echo '#e6e6e6'; else echo 'green'?>" stroke="#000" stroke-width="2" />
+                <rect id="solderpot-rect" x="650" y="195" width="300" height="165" fill="<?php if ($data["solderPot"] == "Close") echo '#e6e6e6'; else echo 'green'?>" stroke="#000" stroke-width="2" />
 
-                <rect id="unused-rect"x="100" y="380" width="80" height="30" fill="gray" stroke="#000" stroke-width="4" />
-                <rect id="close-rect"x="300" y="380" width="80" height="30" fill="#e6e6e6" stroke="#000" stroke-width="4" />
-                <rect id="open-rect"x="500" y="380" width="80" height="30" fill="green" stroke="#000" stroke-width="4" />
+                <rect id="unused-rect" x="100" y="380" width="80" height="30" fill="gray" stroke="#000" stroke-width="4" />
+                <rect id="close-rect" x="300" y="380" width="80" height="30" fill="#e6e6e6" stroke="#000" stroke-width="4" />
+                <rect id="open-rect" x="500" y="380" width="80" height="30" fill="green" stroke="#000" stroke-width="4" />
 
                 <!-- machine section label -->
                 <text class="section-label" id="preheatT1-label" x="75" y="120">Preheat T1</text>
@@ -190,6 +226,30 @@
 
             </svg>
         </div>
+
 </body>
+<script type="text/javascript">
+    window.onload = function() {
+        printTime();
+        setInterval(printTime, 1000);
+        // printSensorData();
+        // setInterval(printSensorData, 120000);
+    }
+
+    function printTime() {
+
+        var d = document.getElementById('Date');
+        var date = new Date();
+        var year = date.getFullYear();
+        var mon = ("0" + (date.getMonth() + 1)).slice(-2);
+        var da = ("0" + (date.getDate())).slice(-2);
+        var day = date.getDay();
+        var h = ("0" + (date.getHours())).slice(-2);
+        var m = ("0" + (date.getMinutes())).slice(-2);
+        var s = ("0" + (date.getSeconds())).slice(-2);
+        var ary = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        d.innerHTML = year + '-' + mon + '-' + da + ' ' + ' ' + h + ':' + m + ':' + s + '  ' + ary[day];
+    }
+</script>
 
 </html>
